3e435e621616f2a0c3c5b66f8e406f600bc45fe2
eabbas <cycl1m@yahoo.com>
2023-01-31 19:51:23 +0330

    refactored with new conditions

--- app/Repositories/LogRepository.php
+++ app/Repositories/LogRepository.php
@@ -1,33 +1,36 @@
 <?php
 
 namespace App\Repositories;
+
 use App\Repositories\Interfaces\LogRepositoryInterface;
 use App\Models\LogFile;
 
 class LogRepository implements LogRepositoryInterface
 {
     //insert log info into database
-    public function create(array $logInfo):bool{
+    public function create(array $logInfo): bool
+    {
         $result = LogFile::firstOrCreate([
-                "service_name" => $logInfo['service_name'],
-                "status_code"  => $logInfo['status_code'],
-                "route_name"       => $logInfo['route_name'],
-                "method_type"        => $logInfo['method_type'],
-                "loged_date" => $logInfo['loged_date']
-            ]);
-        if($result) return true;
+            "service_name" => $logInfo['service_name'],
+            "status_code" => $logInfo['status_code'],
+            "route_name" => $logInfo['route_name'],
+            "method_type" => $logInfo['method_type'],
+            "loged_date" => $logInfo['loged_date']
+        ]);
+
+        return !!$result;
     }
 
     //searches log info by filters and returns count as json string
-    public function showLogsCount(array $filters):string{
+    public function showLogsCount(array $filters): string
+    {
         $filtersQuery = LogFile::query();
-        foreach ($filters as $key=>$filter){
-            if($key == 'serviceNames') $filtersQuery->where('service_name',$filter);
-            if($key == 'statusCode') $filtersQuery->where('status_code',$filter);
-            if($key == 'startDate') $filtersQuery->where('loged_date' , '>=',$filter);
-            if($key == 'endDate') $filtersQuery->where('loged_date' , '<=' ,$filter);
-        }
-        $result['count'] = $filtersQuery->count();
-        return json_encode($result) ;
+
+        if(isset($filters['serviceNames'])) $filtersQuery->where('service_name', $filters['serviceNames']);
+        if(isset($filters['statusCode'])) $filtersQuery->where('status_code', $filters['statusCode']);
+        if(isset($filters['startDate'])) $filtersQuery->where('loged_date','>=', $filters['startDate']);
+        if(isset($filters['endDate'])) $filtersQuery->where('loged_date','<=', $filters['endDate']);
+
+        return json_encode($filtersQuery->count());
     }
 }
