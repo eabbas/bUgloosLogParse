9d7fc1bf6fe2dd0db62b4ec0764acf7eb1d3e570
eabbas <cycl1m@yahoo.com>
2023-01-31 15:16:12 +0330

    route refactored

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('logs/count' , 'App\Http\Controllers\LogSearchController@showLogsCount')->where('query','.+');
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+
+Route::get('logs/count', 'App\Http\Controllers\LogSearchController@showLogsCount');
